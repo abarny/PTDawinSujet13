0139149f20d03086f1a81ec2329f4a49d1cf5044
Hadrien Tran <hadrien.tran@voila.fr>
2015-01-23 09:55:48 +0100

    Inscription
    Première version du formulaire d'inscription

--- Header/header.php
+++ Header/header.php
@@ -4,8 +4,8 @@
 		<meta charset='utf-8'>
 		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
-		<meta name='description' content=''>
-		<meta name='author' content=''>
+		<meta name='description' content='Header du site'>
+		<meta name='author' content='Hadrien'>
 		<link rel='icon' href=''>
 
 		<title>Header</title>
---
+++ Inscription/inscription.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<link href='inscription.css' rel='stylesheet' />
+	
+	<title>Inscription</title>
+</head>
+
+<body>
+
+	<form method="post">
+	
+		<div class="text">
+			<input type="text" name="nom" value="Nom"/>
+		</div>
+		
+		<div class="text">
+			<input type="text" name="prenom" value="Prénom"/>
+		</div>
+		
+		<div class="text">
+			<input type="text" name="login" value="Nom d'utilisateur"/>
+		</div>
+		
+		<div class="text">
+			<input type="text" name="mail" value="Adresse mail"/>
+		</div>
+		
+		<div class="text">
+			<input type="text" name="mdp1" value="Mot de passe"/>
+		</div>
+		
+		<div class="text">
+			<input type="text" name="mdp2" value="Confirmer le mot de passe"/>
+		</div>
+		
+		
+		<div class="button">
+			<input type="submit" value="S'inscrire"/>
+			<input type="reset" value="Effacer"/>
+		</div>
+		
+	</form>
+	
+</body>
+
+</html>
