ef539a5b9929fb81b123902df8cf36a7d367ff9a
Hadrien <hadrien.tran@voila.fr>
2015-02-05 09:08:26 +0100

    Correction finale BDD
    Passage des champs en utf-8_general (au lieu de latin ou de utf_8_bin)

--- BDD/BDD.sql
+++ BDD/BDD.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.4
+-- version 3.4.11.1deb2+deb7u1
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Jeu 05 Février 2015 à 08:38
--- Version du serveur :  5.6.15-log
--- Version de PHP :  5.4.24
+-- Client: info-arie.iut.bx1
+-- Généré le: Jeu 05 Février 2015 à 09:06
+-- Version du serveur: 5.5.41
+-- Version de PHP: 5.4.36-0+deb7u3
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `teamshare`
+-- Base de données: `info_hatran`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `groupes` (
   `id_groupe` int(11) NOT NULL AUTO_INCREMENT,
   `nom_groupe` varchar(32) NOT NULL,
   PRIMARY KEY (`id_groupe`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `groupes` (
 CREATE TABLE IF NOT EXISTS `membres_taches` (
   `id_util` int(11) NOT NULL,
   `id_tache` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -51,16 +51,16 @@ CREATE TABLE IF NOT EXISTS `membres_taches` (
 
 CREATE TABLE IF NOT EXISTS `taches` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `title` varchar(255) NOT NULL,
   `start` date NOT NULL,
   `end` date NOT NULL,
-  `description` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `description` text NOT NULL,
   `heures_estim` float NOT NULL,
-  `responsable` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `responsable` varchar(255) NOT NULL,
   `id_groupe` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_groupe` (`id_groupe`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `taches`
@@ -81,29 +81,33 @@ INSERT INTO `taches` (`id`, `title`, `start`, `end`, `description`, `heures_esti
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id_user` int(255) NOT NULL,
+  `id_user` int(255) NOT NULL AUTO_INCREMENT,
   `prenom` varchar(255) NOT NULL,
   `nom_user` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `mail` varchar(255) NOT NULL,
   `pass` varchar(255) NOT NULL,
   `color` int(255) NOT NULL,
-  `droits_admin` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `droits_admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1 = Admin ; 0 = Membre',
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `mail` (`mail`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Contenu de la table `users`
 --
 
 INSERT INTO `users` (`id_user`, `prenom`, `nom_user`, `username`, `mail`, `pass`, `color`, `droits_admin`) VALUES
-(0, 'alexis', 'barny', 'alexrider45', 'barny.alexis@gmail.com', '1d220268dba9d114e4c1b0f833041feb', 1, 0),
+(1, 'Naro', 'Vagrant', 'LeVagabond', 'Naro.Vagrant@Etherval.fr', 'splitworld', 1, 1),
 (2, 'Lilith', 'Godspell', 'LightAndDarkness', 'Lillith@Etherval.fr', 'dragon', 2, 0),
 (3, 'Ike', 'SouthIsland', 'Soldier-Monk', 'Ike@Etherval.fr', 'lover', 3, 0),
 (4, 'Sherenne', 'Capitale', 'Archer', 'Sherenne@Etherval.fr', 'firingshot', 4, 0),
 (5, 'Ilo', 'Capitale', 'Kaisether', 'Ilo@Etherval.fr', 'ether', 5, 0),
 (6, 'Tom', 'Veldyon', 'Chimera', 'Tom.Veldyon@Gaïa.fr', 'gunblade', 6, 0),
-(7, 'Naïl', 'Unknown', 'Smémancien', 'Naïl@Etherval.fr', 'rock', 7, 0);
+(7, 'Naïl', 'Unknown', 'Smémancien', 'Naïl@Etherval.fr', 'rock', 7, 0),
+(8, 'Elise', 'Tesaross', 'ChainChimera', 'Elise.Tesaross@Gaïa.fr', 'Tom<3', 8, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
