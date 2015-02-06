b7a6e7aa9c7cedd5e906c9853615c16f3967d998
Hadrien <hadrien.tran@voila.fr>
2015-02-06 10:43:39 +0100

    Connexion (réparé)
    Header réparé

--- PHP/header.php
+++ PHP/header.php
@@ -96,29 +96,23 @@
 						<div id='navbar' class='navbar-collapse collapse' ng-controller="HeaderController">
 							<ul class='nav navbar-nav'>
 								<li id='home'><a href='calendar.php'>Accueil</a></li>
-
-
+								
 								<?php
-									/* A afficher si l'utilisateur est connecté */
 									if (isset($_SESSION['username'])){
 										echo("<li id='dragndrop'><a href='dragjquery2.php'>Gestion des tÃ¢ches</a></li>");
-										/* A afficher si l'utilisateur est Admin */
-										if ($_SESSION['droits_admin'] == 1)
-											echo("<li id='gestuser'><a href='gestionUsers.php'>Gestion des utilisateurs</a></li>);
+										echo("<li id='gestuser'><a href='gestionUsers.php'>Gestion des utilisateurs</a></li>");
 									}
 								?>
-								
 								<li id='help'><a href='#'>Aide</a></li>
 							</ul>
 							<ul class='nav navbar-nav navbar-right'>
 							
+
 								<?php
-									/* A afficher si l'utilisateur est connecté */
 									if (isset($_SESSION['username'])){
 										echo("<li id='gestcmpt'><a href='gestionCompte.php'>Gestion du compte</a></li>");
-										echo("<li><a href=''>Se déconnecter</a></li>");
+										echo("<li><a href=''>Se dÃ©connecter</a></li>");
 									}
-									/* A afficher si l'utilisateur est déconnecté */
 									else {
 										echo("<li id='register'><a href='inscription.php'>S'inscrire</a></li>");
 										echo("<li><a href='javascript:ouvrirPopup()'>Se connecter</a></li>");
@@ -137,9 +131,7 @@
 					
 					<input type="submit" name="connect" value="Connexion" />
 					<img alt="Fermeture popup" src="../img/fermeture.png" onclick="fermerPopup()" />
-					
-					<?php include('formulaireConnexion.php'); ?>
-				
+									
 				</div>
 				
 			</div> <!-- /container -->
