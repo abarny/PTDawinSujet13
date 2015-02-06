28034136d96fde83feccf19202141b826030ddd8
Barny <barny.alexis@gmail.com>
2015-02-06 10:39:52 +0100

    curseur sur calendrier

--- PHP/calendar.php
+++ PHP/calendar.php
@@ -5,6 +5,7 @@
 		<link rel="stylesheet" href="fullcalendar/fullcalendar.css" />
 		<script src="fullcalendar/lib/jquery.min.js"></script> 
 		<script src="fullcalendar/lib/moment.min.js"></script>
+		<script src='fullcalendar/lib/jquery-ui.custom-datepicker.js'></script>
 		<script src="fullcalendar/fullcalendar.js"></script>
 		<script src='fullcalendar/lang/fr.js'></script>
 		<script>
@@ -21,14 +22,23 @@ $(document).ready(function() {
     $("#calendar").fullCalendar({
         // put your options and callbacks here
         height: "auto",
+        lang: 'fr',
 //        buttonText: {today: "retour au mois en cours"},
         fixedWeekCount: false,
-		events: "http://localhost:8080/PHP/events.php",
+		events: "http://localhost:9292/PHP/events.php",
 
         dayClick: function(date, jsEvent, view) {
         var date = date.format();
 		$(location).attr('href',"detailjour.php?date="+date);
-    }
+    	},
+
+    	eventClick: function(date, event) {
+    	var date = date.format();
+        alert(event.date);
+            return false;
+        }
+    	
+
     });
 
 });		</script>
--- PHP/connect.php
+++ PHP/connect.php
@@ -3,17 +3,17 @@
 	// session_start();
 	
 	// Connexion a la BDD
-	$host = "info-arie";
-	$user = "hatran";
-	$pass = "9JyGX1PHZdAp";
-	$bdd = "info_hatran";
+	$host = "127.0.0.1";
+	$user = "root";
+	$pass = "";
+	$bdd = "teamshare";
 	
 	// Connexion
 	@mysql_connect($host,$user,$pass) or die("Impossible de se connecter");
 	@mysql_select_db("$bdd") or die("Impossible de se connecter");
 	
 	try {
-		$pdo = new PDO('mysql:host=info-arie;dbname=info_hatran', 'hatran', '9JyGX1PHZdAp');
+		$pdo = new PDO('mysql:host=127.0.0.1;dbname=teamshare', 'root', '');
 	}
 	catch(Exception $e) {
 		die('Erreur : ' . $e->getMessage());
